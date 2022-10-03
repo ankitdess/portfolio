c5f854c275fc1bd1508b99de8714f3f1f5eaf78f
ankitdess <sendtoankitzz@gmail.com>
2022-10-03 18:57:31 +0530

    s

--- app.js
+++ app.js
@@ -3,3 +3,26 @@ $('#menu').click(function(){
 		$('#navlinks').toggleClass('visible');
 	});
 
+const body=document.body;
+let lastscroll=0;
+
+window.addEventListener('scroll',()=>{
+	const currentscroll=window.pageYOffset;
+
+	if(currentscroll<=0){
+		body.classList.remove('scrollup')
+	}
+
+	if(currentscroll>lastscroll && !body.classList.contains("scrolldown")){
+		body.classList.remove("scrollup");
+		body.classList.add("scrolldown")
+	}
+
+
+	if(currentscroll<lastscroll && body.classList.contains("scrolldown")){
+		body.classList.remove("scrolldown");
+		body.classList.add("scrollup")
+	}
+
+	lastscroll=currentscroll;
+})
