24152f3abdcbf10123b66d979fea736a8a53289f
ankitdess <sendtoankitzz@gmail.com>
2022-10-29 20:49:34 +0530

    s

--- app.js
+++ app.js
@@ -8,6 +8,30 @@ $('#mob').click(function(){
 	$('#navlinks').removeClass("visible");
 })
 
+var text = ["Get set Go..", "This Shouldn't take much time", "Hang on few sec.."];
+var counter = 0;
+var elem = document.getElementById("changeText");
+var inst = setInterval(change, 3000);
+
+/*Loading animation*/
+function loader(){
+	var y=document.querySelector('.box');
+	var x= document.getElementById('preload');
+	document.body.style.overflow="unset"
+	y.style.display="none";
+  
+   setTimeout(()=>{
+	x.style.opacity="0";
+	x.style.transition="1s"
+   },200) 
+  
+	  setTimeout(()=>{
+	  x.style.display="none";
+	},1000)
+  }
+
+  
+
 const body=document.body;
 let lastscroll=0;
 
